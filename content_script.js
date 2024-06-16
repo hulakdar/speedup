907ff41d03b5ddf9e7f68aaf69cd6ae282b6e804
hulakdar <skamoza@gmail.com>
2024-06-16 15:13:19 +0300

    Made work with twitch, added hotkeys for speed selection

--- content_script.js
+++ content_script.js
@@ -1,10 +1,36 @@
 function setSpeed(speed)
 {
-    elem = document.getElementsByTagName("video")[0]
-    if(elem)
-    {
-        elem.playbackRate=speed
-    }
+  elem = document.getElementsByTagName("video")[0]
+  if(elem)
+  {
+    elem.playbackRate=speed
+  }
 }
 
-setSpeed(2.5)
+setSpeed(3)
+
+function setHighSpeed()
+{
+  setSpeed(3)
+}
+function setMidSpeed()
+{
+  setSpeed(2)
+}
+function setLowSpeed()
+{
+  setSpeed(1)
+}
+
+if (window.attachEvent) {window.attachEvent('onload', setHighSpeed);}
+else if (window.addEventListener) {window.addEventListener('load', setHighSpeed, false);}
+else {document.addEventListener('load', setHighSpeed, false);}
+
+document.addEventListener('keydown', function(event) {
+  if (event.altKey)
+  {
+    if (event.key === '3') setHighSpeed()
+    if (event.key === '2') setMidSpeed()
+    if (event.key === '1') setLowSpeed()
+  }
+});
